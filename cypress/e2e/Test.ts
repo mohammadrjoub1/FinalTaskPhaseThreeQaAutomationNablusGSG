5cfb532d5ffe5045b0c1252d5d5e2ecbb698136e
mohammad rjoub <122231941+mohammadrjoub1@users.noreply.github.com>
2023-11-11 17:49:32 +0200

    Update Test.ts

--- cypress/e2e/Test.ts
+++ cypress/e2e/Test.ts
@@ -1,6 +1,7 @@
 import { Given, When, Then } from "@badeball/cypress-cucumber-preprocessor";
 
 //this is beforeEach
+//okok
 Given("Visit the home page", () => {
   cy.login("Admin", "admin123");
   
